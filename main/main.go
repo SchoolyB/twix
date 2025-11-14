e89447ea862bff54c7c2077e8ed5aa28665f9d7c
SchoolyB <marshallbcodes@gmail.com>
2025-11-14 08:37:47 -0600

    chore: update main/add helpers for creating post name

--- main/main.go
+++ main/main.go
@@ -4,11 +4,11 @@ import (
 	"fmt"
 
 	// c "client" //uncomment when ready to use client functions
+	"ostrichdb-go/src/lib"
 	sdk "ostrichdb-go/src/sdk"
 )
 
 func main (){
-
     config:= sdk.NewConfigBuilder()
     client:= sdk.NewClientBuilder(config)
 
@@ -28,4 +28,26 @@ func main (){
      	fmt.Println("Error creating collection")
     }
 
+
+    //Any time the user creates a Post, a new cluster will be created
+    newPostName:=create_post_name(collection)
+    cluster:= sdk.NewClusterBuilder(project, collection, newPostName)
+    sdk.CreateCluster(cluster)
+
+}
+
+
+//Helper functions for client will move later - Marshalla
+func increment_post_count(collection *lib.Collection) int{
+	currentCount:= sdk.GetClusterCount(collection)
+	incremented := currentCount + 1
+	return incremented
+}
+
+
+//Only used to create a name for the post within OstrichDB e.g Post1, Post2, etc
+func create_post_name(collection *lib.Collection) string{
+	postCount:= increment_post_count(collection)
+	postName:= fmt.Sprintf("Post%d", postCount)
+	return postName
 }
