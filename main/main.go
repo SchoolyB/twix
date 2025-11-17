5b23123b746e31a5020c1b1011cc5571dc724619
SchoolyB <marshallbcodes@gmail.com>
2025-11-16 19:52:49 -0600

    chore: update main

--- main/main.go
+++ main/main.go
@@ -1,38 +1,57 @@
 package main
 
 import (
-	c "client" //uncomment when ready to use client functions
-	// "fmt"
+	"fmt"
 	"ostrichdb-go/src/lib"
+	c "client" //uncomment when ready to use client functions
 	sdk "ostrichdb-go/src/sdk"
 )
+
 var config lib.Config = *sdk.NewConfigBuilder()
 var client lib.Client = *sdk.NewClientBuilder(&config)
 var project lib.Project =  *sdk.NewProjectBuilder(&client, "twix")
 
 
 func main (){
+	var err error
 
-    // projectErr:= sdk.CreateProject(&project)
-    // if projectErr != nil {
-    //  	fmt.Println("Error creating project")
-    //   	fmt.Println(projectErr)
-    // }
+	// START | START | START | START | START | START | START | START | START
+	// The following code is an example of the flow for creating a
+	// Project(Dir of Users), Specific User Collection, and a first Post(Cluster)
+	// Once 'END' is reached an exmaple of another flow begins
 
-    //Note: Whenever implementing user auth on client please replace "Marshall" with username input
+    err= sdk.CreateProject(&project)
+    if err != nil {
+     	fmt.Println("Error creating project")
+      	fmt.Println("Error: ",err)
+    }
+
+    // //Note: Whenever implementing user auth on client please replace "Marshall" with username input
     collection:= sdk.NewCollectionBuilder(&project, "Marshall")
-    // collectionErr:= sdk.CreateCollection(collection)
+    err = sdk.CreateCollection(collection)
 
-    // if collectionErr != nil {
-    //  	fmt.Println("Error creating collection")
-    // }
+    if err != nil {
+     	fmt.Println("Error creating Collection")
+      	fmt.Println("Error: ",err)
+    }
 
     //Testing things out
     currentUser:= c.NewUserBuilder("@Marshall")
+    //TODO: If there is a space in the string value of Content is not stored. OstrichDB engine issue
     newPost:= c.NewPostBuilder(&currentUser, "Hello World")
-    c.HandlePost(project, *collection, newPost)
+
+    err = c.HandlePost(*collection, newPost)
+    if err != nil {
+   		fmt.Println("Error appending new post to a Collection")
+    	fmt.Println("Error: ",err)
+    }
+    // END | END| END| END| END| END| END| END| END| END
 
 
+
+    // Fetching a Post from OstrichDB
+    specificPost:= sdk.NewClusterBuilder(collection, "Marshall")
+    fmt.Println(c.FetchPostAuthor(specificPost, 1))
     // c.Run()
 
 }
